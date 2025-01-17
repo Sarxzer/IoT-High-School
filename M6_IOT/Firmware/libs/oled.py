c4b733a5549aca5f4abb1a2c3ede7501f2718d23
Sarxzer <80457483+Sarxzer@users.noreply.github.com>
2025-01-17 10:57:27 +0100

    Add configuration files and initial implementations for WiFi and OLED display functionalities

---
+++ M6_IOT/Firmware/libs/oled.py
@@ -0,0 +1,34 @@
+from machine import SoftI2C, Pin
+import time
+import ssd1306
+
+class Oled:
+    """
+    Class to interact with the OLED display with a ssd1306 driver.
+    """
+
+    def __init__(self, scl: int, sda: int, width: int, height: int):
+        """
+        Constructor of the class.
+        """
+        self.i2c = SoftI2C(scl=Pin(scl), sda=Pin(sda), freq=100000, timeout=5000)
+        self.oled = ssd1306.SSD1306_I2C(width, height, self.i2c)
+    
+    def text(self, text: str, x: int, y: int):
+        """
+        Display text on the OLED display.
+        """
+        self.oled.text(text, x, y)
+    
+    def show(self):
+        """
+        Show the content of the OLED display.
+        """
+        self.oled.show()
+    
+    def fill(self, fill: int):
+        """
+        Fill the OLED display with a color.
+        """
+        self.oled.fill(fill)
+        
