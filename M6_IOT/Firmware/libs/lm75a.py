bb03ded8cba3bf9f1332699ccdfafdcdc5e8cecb
Sarxzer <nathanvaillendet@gmail.com>
2025-03-04 13:58:17 +0100

    Refactor LM75 sensor reading to improve temperature conversion and add debugging output

--- M6_IOT/Firmware/libs/lm75a.py
+++ M6_IOT/Firmware/libs/lm75a.py
@@ -18,12 +18,26 @@ class lm75a:
         Read temperature from LM75 sensor
         """
         try:
-            buf = bytearray(1)
-            buf[0] = 0x00
-            self.i2c.writeto(self.addr, buf)
-            mesure = self.i2c.readfrom(self.addr, 2)
-            val = (mesure[0] << 3)|(mesure[1] >> 5)
-            temperature = val * 0.125
+            buf = bytearray(1) # création d’un buffer 1 octet
+            buf[0] = 0x00 # adresse du registre point
+            self.i2c.writeto(0x48, buf) # écriture dans le LM75
+            mesure = self.i2c.readfrom(0x48, 2) # lecture
+            
+            mesure_int = (mesure[0] << 8) | mesure[1]
+            
+            print("temperature en hex:", hex(mesure_int))
+            print("temperature en bin:", bin(mesure_int))
+
+            # Convert to 11-bit signed value
+            mesure = mesure_int >> 5  # Reduction de 16 à 11 bits
+
+            if (mesure > 1023):
+                res = (mesure ^ 0b11111111111)+1
+                temperature = - res * 0.125
+            else :
+                temperature = mesure * 0.125
+            print("temperature:",temperature)
+            time.sleep(3)
             return temperature
         except Exception as e:
             print('Error reading LM75 sensor:', e)
